c9a3fef6e45e07954519d20b1db73a9aaeb93622
Benjietorga7 <Benjietorga7@gmail.com>
2022-10-28 19:24:42 +0800

    Added  gallery page and navbar

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
 });
